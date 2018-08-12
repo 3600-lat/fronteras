1ef27cd6f7404faad0461e241f7da39582a0cc79
Sylvain Lesage <severo@rednegra.net>
2018-08-12 12:26:40 -0400

    Aplicar prettier con eslint. Modif nombre Departamentos

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Departamentos from '@/components/Departamentos'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Departamentos',
+      component: Departamentos
     }
   ]
 })
