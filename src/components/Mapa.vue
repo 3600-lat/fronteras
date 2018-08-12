95e653a14afa1af177ffe93e75a8fa82e4469b1b
Sylvain Lesage <severo@rednegra.net>
2018-08-12 15:11:57 -0400

    Mostrar los departamentos

--- src/components/Mapa.vue
+++ src/components/Mapa.vue
@@ -1,6 +1,6 @@
 <template>
-  <svg class="mapa" width="500px" height="500px">
-    <circle cx="50" cy="50" r="50"/>
+  <svg class="mapa" :width='width' :height='width'>
+    <path class="polygons" />
   </svg>
 </template>
 
@@ -11,8 +11,9 @@ export default {
   name: 'Mapa',
   data() {
     return {
-      contenido: 'Un círculo',
-      geojson2:
+      height: 500,
+      width: 500,
+      geojson:
         'https://raw.githubusercontent.com/severo/data/master/20180331/departamentos_simplified_002.geojson'
     }
   },
@@ -21,11 +22,12 @@ export default {
   },
   methods: {
     initialize() {
-      const projection = d3.geoMercator()
-      const path = d3.geoPath().projection(projection)
-      const svg = d3.select('svg.mapa')
-      d3.json(this.geojson2).then(function(data) {
-        svg.append('path').attr('d', path(data))
+      const height = this.height
+      const width = this.width
+      d3.json(this.geojson).then(function(data) {
+        const projection = d3.geoMercator().fitSize([height, width], data)
+        const path = d3.geoPath().projection(projection)
+        d3.select('svg.mapa path.polygons').attr('d', path(data))
       })
     }
   }
@@ -34,7 +36,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-circle {
-  stroke: red;
+.mapa {
+  margin: 2em;
+}
+
+.polygons {
+  stroke: black;
+  stroke-width: 0.5px;
+  fill: none;
 }
 </style>
