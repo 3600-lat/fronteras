1520d527db4f033473d6b0dd5d99adab91b29174
Sylvain Lesage <severo@rednegra.net>
2018-08-12 12:41:24 -0400

    Fix tests

--- test/unit/specs/Departamentos.spec.js
+++ test/unit/specs/Departamentos.spec.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import Departamentos from '@/components/Departamentos'
 
-describe('HelloWorld.vue', () => {
+describe('Departamentos.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
+    const Constructor = Vue.extend(Departamentos)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent).toEqual(
-      'Welcome to Your Vue.js App'
+      'Departamentos'
     )
   })
 })
